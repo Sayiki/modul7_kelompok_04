3cfbbda46338d69b4fa1bf5f857fe5ed2f643a36
Triani Putri Mumpuni <127008970+trianipm@users.noreply.github.com>
2023-04-05 17:26:41 +0700

    menambahkan class GlossaryItem1302213091

---
+++ GlossaryItem1302213091.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal7_1302213091_Kelompok_4
+{
+    internal class GlossaryItem1302213091
+    {
+    }
+}
