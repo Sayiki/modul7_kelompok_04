9f7a385483a6091018b82579d4d3543a189efd35
Bengley123 <yesasihombing1@gmail.com>
2023-04-05 17:28:55 +0700

    Menambahkan data Mahasiswa

---
+++ modul7_kelompok_04/DataMahasiswa_1302213108.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_04
+{
+    public class DataMahasiswa_1302213108
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public Fulladress adr { get; set; }
+        public Fullcourses course { get; set; }
+
+        public DataMahasiswa_1302213108()
+        {
+
+        }
+
+        public class Fulladress
+        {
+            public string streetadress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+
+            public Fulladress(string streetadress, string city, string state)
+            {
+                this.streetadress = streetadress;
+                this.city = city;
+                this.state = state;
+            }
+
+            public Fulladress()
+            {
+
+            }
+        }
+
+        public class Fullcourses
+        {
+            public string name { get; set; }
+            public string code { get; set; }
+
+            public Fullcourses(string name, string code)
+            {
+                this.name = name;
+                this.code = code;
+            }
+
+            public Fullcourses()
+            {
+
+            }
+        }
+    }
+}
--- modul7_kelompok_04/Program.cs
+++ modul7_kelompok_04/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul7_kelompok_04;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa_1302213108 data = new DataMahasiswa_1302213108();
+        String jsonString = File.ReadAllText("C:\\Users\\PRAKTIKAN\\Downloads\\modul7_kelompok4\\modul7_kelompok_04\\bin\\Debug\\net6.0\\jurnal7_1_1302213108.json");
+        data = JsonSerializer.Deserialize<DataMahasiswa_1302213108>(jsonString);
+        Console.WriteLine("Nama "+ data.firstname+ data.lastname + "Berjenis Kelamin "+ data.gender + "Berumur" + data.age+ "dan BerAlamat di" + data.adr+ "dan Mengambil mata kuliah"+ data.course);
+
+    }
+}
