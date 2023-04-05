e13fe35753b7bcb9ae85db7b9cc97607cef34087
Sayiki <arzaqajradika1@gmail.com>
2023-04-05 17:03:11 +0700

    menambahkan class DataMahasiswa_1302210096

---
+++ modul7_kelompok_04/DataMahasiswa_1302210096.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_04
+{
+    public class DataMahasiswa_1302210096
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public adr address { get; set; }
+        public List<kurse> courses { get; set; }
+
+        public DataMahasiswa_1302210096()
+        {
+
+        }
+
+        public class adr
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+
+            public adr(string streetAddress, string city, string state)
+            {
+                this.streetAddress = streetAddress;
+                this.city = city;
+                this.state = state;
+            }
+
+            public adr()
+            {
+
+            }
+        }
+
+        public class kurse
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+
+            public kurse(string kode, string nama){
+                this.code = kode;
+                this.name = nama;
+            }
+            public kurse()
+            {
+
+            }
+
+        }
+
+
+        public DataMahasiswa_1302210096(string firstName, string lastName , string gender, int age, adr address, List<kurse> courses)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gender = gender;
+            this.age = age;
+            this.address = address;
+            this.courses = courses;
+        }
+
+
+
+    }
+}
--- modul7_kelompok_04/Program.cs
+++ modul7_kelompok_04/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using modul7_kelompok_04;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa_1302210096 data = new DataMahasiswa_1302210096();
+
+        String jsonString = File.ReadAllText("C:\\Users\\PRAKTIKAN\\Documents\\modul7_kelompok_04\\modul7_kelompok_04\\bin\\Debug\\net6.0\\jurnal7_1_1302210096.json");
+
+        DataMahasiswa_1302210096 mhs = JsonSerializer.Deserialize<DataMahasiswa_1302210096>(jsonString);
+        Console.WriteLine("Nama " + mhs.firstName + " " + mhs.lastName + " " + mhs.gender + " " + mhs.age + " " + mhs.address);
+
+        for (int i = 0; i < mhs.courses.Count; i++)
+        {
+            Console.WriteLine("Course " + i);
+            Console.WriteLine(mhs.courses[i].code);
+            Console.WriteLine(mhs.courses[i].name);
+            Console.WriteLine("");
+        }
+    }
+
+}
