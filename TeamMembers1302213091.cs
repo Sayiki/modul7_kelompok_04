61f1f70c65e597faa0663e59a6f6b5813af9ab34
Triani Putri Mumpuni <127008970+trianipm@users.noreply.github.com>
2023-04-05 17:25:29 +0700

    menambahkan class TeamMembers1302213091

---
+++ TeamMembers1302213091.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal7_1302213091_Kelompok_4
+    public class DataMahasiswa1302213091
+{
+    public Fullname nama { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public int nim { get; set; }
+    public DataMahasiswa1302213091()
+    {
+
+    }
+    public class Fullname
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public Fullname(string depan, string belakang)
+        {
+            this.firstName = depan;
+            this.lastName = belakang;
+        }
+        public Fullname()
+        {
+        }
+    }
+    public DataMahasiswa1302213091(Fullname nama, string gender, int age, int nim)
+    {
+        this.nama = nama;
+        this.gender = gender;
+        this.age = age;
+        this.nim = nim;
+    }
+}
